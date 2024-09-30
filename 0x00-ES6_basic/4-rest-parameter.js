aecbb599556ac716e99fe33ceea44465e3a2b555
Idris Elgarrab <franckenstein999@gmail.com>
2024-09-30 22:45:59 +0100

    file(4-rest-parameter.js): Add 4-rest-parameter.js

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,5 +1,5 @@
 export default function returnHowManyArguments(...args) {
-    let sum = 0;
-    for (let arg of args) sum++;
-    return sum;
+  let sum = 0;
+  for (let arg of args) sum++;
+  return sum;
 }
